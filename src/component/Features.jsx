79dcf56bd7f459ce0db50e61e8783c632ce6fcc5
akhileshBhardwaj <rajbharakhilesh818@gmail.com>
2026-06-17 11:32:07 +0530

    update

--- src/component/Features.jsx
+++ src/component/Features.jsx
@@ -21,7 +21,7 @@ const Features = () => {
       </div>
 
       {/* Products */}
-      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-5">
         {Product.slice(0, 6).map((item) => (
           <div
             key={item.id}
